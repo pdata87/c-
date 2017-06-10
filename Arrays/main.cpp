da0587b1c96123a220a7f2b1d41665500c3017ad
pdata <pdata87@o2.pl>
2017-06-10 11:07:38 +0200

    added implementation of merging two arrays in C style

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -2,9 +2,10 @@
 #include <array>
 #include <algorithm>
 #include <vector>
+#include <cstring>
 
 void revert_array(std::array<int,20> & arr);
-
+int *mergeTwoArrays(int *first, int *second, int numOfElements);
 std::ostream&  operator <<(std::ostream& stream, const std::array<int,20>& arr) {
 
    for(int elem : arr){
@@ -15,6 +16,22 @@ std::ostream&  operator <<(std::ostream& stream, const std::array<int,20>& arr) 
 }
 // 1 .Write a program to concatenate two tables.
 int main() {
+
+    //C array style
+
+    int firstCarray[10] = { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+    int secondCarray[10] = { 6, 6, 6, 6, 6, 6, 6, 6, 6, 6 };
+    int numOfElements = sizeof(firstCarray)/sizeof(int) +  sizeof(firstCarray)/sizeof(int);
+    int * output = mergeTwoArrays(firstCarray, secondCarray, numOfElements);
+
+
+
+    for(int i =0; i<numOfElements;i++) {std::cout << output[i] << std::endl;};
+
+
+    // C++ Style
+    std:: cout <<  "C++ Implementation - merge and reverse " <<std:: endl;
+
     std::array<int,10> firstArray {1,2,3,4,5,6,7,8,9,0};
     std::array<int,10> secondArray {0,0,0,0,0,0,0,0,0,0};
     std::array<int,20> mergedArray;
@@ -22,6 +39,8 @@ int main() {
 
     std::merge(firstArray.begin(),firstArray.end(),secondArray.begin(),secondArray.end(),mergedArray.begin());
     std::cout <<"Before reverse "<< mergedArray << std::endl; ;
+
+    // Passing by reference
     revert_array(mergedArray);
 
 
@@ -35,3 +54,19 @@ int main() {
 void revert_array(std::array<int,20> & arr){
         std::reverse(arr.begin(),arr.end());
 }
+
+
+int * mergeTwoArrays(int *first, int *second, int numOfElements) {
+
+    int elements_in_each_array = numOfElements/2;
+
+    int * outputArray = (int*)malloc(numOfElements * sizeof(int));
+
+        std::memcpy(&outputArray[0],first,elements_in_each_array * sizeof(int));
+        std::memcpy(&outputArray[elements_in_each_array],second,elements_in_each_array* sizeof(int));
+
+    return outputArray;
+
+}
+
+
